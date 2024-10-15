82f3fb579fa929eccffcbbbf64dd1da012fba214
FrLars21 <149402342+FrLars21@users.noreply.github.com>
2024-10-16 00:34:54 +0200

    changes

--- SincNetModel.py
+++ SincNetModel.py
@@ -33,7 +33,7 @@ class SincNetConfig:
 
     # number of neurons
     fc_layers: List[int] = (
-        # 768,
+        768,
         768,
         768,
     )
@@ -66,7 +66,7 @@ class SincNetModel(nn.Module):
                     nn.MaxPool1d(cfg.conv_max_pool_len[i]) if cfg.conv_max_pool_len[i] > 1 else nn.Identity(),
                     ChannelwiseLayerNorm(out_channels) if not cfg.conv_layers_batchnorm else nn.BatchNorm1d(out_channels),
                     nn.LeakyReLU(),
-                    nn.Dropout(0.2), # experimental
+                    nn.Dropout(0.3), # experimental
                 ))
             else:
                 in_channels = cfg.conv_layers[i-1][0]
@@ -75,7 +75,7 @@ class SincNetModel(nn.Module):
                     nn.MaxPool1d(cfg.conv_max_pool_len[i]) if cfg.conv_max_pool_len[i] > 1 else nn.Identity(),
                     ChannelwiseLayerNorm(out_channels) if not cfg.conv_layers_batchnorm else nn.BatchNorm1d(out_channels),
                     nn.LeakyReLU(),
-                    nn.Dropout(0.2), # experimental
+                    nn.Dropout(0.3), # experimental
                 ))
 
         # create the fc layers
